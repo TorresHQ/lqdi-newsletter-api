2d244e5a3ed5539a716a17dd6eb530fec88801f5
Héctor Queiroz Torres <hqt2300@gmail.com>
2023-11-02 16:42:39 -0300

    feat(routes): add user registration route to api

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\v1\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::prefix('v1')->group(function (){
+    Route::prefix('users')->group(function () {
+        Route::post('/', [UserController::class, 'store']);
+    });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
 });
