14c16b8b6df44265f3ce3d7bdd9ee17485722410
Héctor Queiroz Torres <hqt2300@gmail.com>
2023-11-02 16:37:31 -0300

    feat(UserService): add UserService

---
+++ app/Services/User/UserService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services\User;
+
+use App\Repositories\User\UserRepository;
+use App\Services\BaseService;
+use Illuminate\Database\Eloquent\Model;
+
+class UserService extends BaseService
+{
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->repository = $userRepository;
+    }
+
+    public function create (array $data): Model
+    {
+        $data['password'] = bcrypt($data['password']);
+        return parent::create($data);
+    }
+
+    public function getUserByEmail(string $login)
+    {
+        return $this->repository->getByEmail($login);
+    }
+}
