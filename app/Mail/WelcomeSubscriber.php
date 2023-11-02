be4ab67cc892ecbd49c0ff4754314ac5f0f9699b
Héctor Queiroz Torres <hqt2300@gmail.com>
2023-11-02 17:15:28 -0300

    feat(Mail): add WelcomeSubscriber mail

---
+++ app/Mail/WelcomeSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class WelcomeSubscriber extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Welcome Subscriber',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.layout_welcome_subscriber',
+        );
+    }
+
+    public function attachments(): array
+    {
+        return [];
+    }
+}
